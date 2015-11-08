ab214fe44ee43d551435b9bbe53acc3a54089452
ZenryokuService <tak4545@outlook.jp>
2015-11-08 21:22:55 +0900

    11/8 Step1-2の問4まで作成完了

--- PracticeJava/src/jp/zenryoku/sample/Step1_2.java
+++ PracticeJava/src/jp/zenryoku/sample/Step1_2.java
@@ -39,4 +39,58 @@ public class Step1_2 {
 		// 計算結果を表示
 		System.out.println("計算結果" + num1);
 	}
+
+	/**
+	 * 問3のサンプルソース
+	 * このメソッドを書き換えずに
+	 * 「edcba」と表示させてください
+	 */
+	public void mondai3() {
+		// char型の配列
+		char[] ch = {'a', 'b', 'c', 'd', 'e'};
+		char[] result = sortChar(ch);
+
+		System.out.println("答え："
+				+ result[0] + result[1] + result[2] + result[3] + result[4]);
+	}
+
+	/**
+	 * 問3の配列をソートするメソッド
+	 * ここのソースをjp.zenryoku.sample以下に
+	 * コピーして作成してください
+	 * @param ch
+	 * @return char[] ソートを完了した配列
+	 */
+	private char[] sortChar(char[] ch) {
+		
+		return ch;
+	}
+
+	/**
+	 * 問4：メソッドhenshuを書き換えて
+	 *     答えに40を表示してください
+	 */
+	public void mondai4() {
+		// int型の配列
+		int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int kekka = 0;
+		// 配列の編集処理
+		nums = henshu(nums);
+		/*
+		 * 繰り返しのFOR文
+		 * 配列を０番目から最後まで繰り返し処理を行う
+		 */
+		for(int i=0; i < nums.length; i++) {
+			kekka += nums[i];
+		}
+		System.out.println("答え：" + kekka);
+	}
+	/**
+	 * 問4の配列を編集するメソッド
+	 * ここのソースをjp.zenryoku.sample以下に
+	 * コピーして作成してください
+	 */
+	private int[] henshu(int[] nums) {
+		return nums;
+	}
 }
