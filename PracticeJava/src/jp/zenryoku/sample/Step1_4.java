567a3bcb38fab93f605eee557e07f3c18b03abfa
ZenryokuService <tak4545@outlook.jp>
2015-12-01 23:14:33 +0900

    12/1 Step1-4記述、一応の形で完了

--- PracticeJava/src/jp/zenryoku/sample/Step1_4.java
+++ PracticeJava/src/jp/zenryoku/sample/Step1_4.java
@@ -1,12 +1,89 @@
 package jp.zenryoku.sample;
 
-/**
+/**　この行からJavaDocコメントです<br/>
  * Step1-4:Stringクラスの扱い<br/>
- * クラス型（オブジェクト型）の変数の扱い、メソッドの扱いの練習
+ * クラス型（オブジェクト型）の変数の扱い、メソッドの扱いの練習<br/>
+ * <br/>
+ * 下にある＠マーク次の行は、JavaDocに付帯情報を表示するためのものです<br/>
+ * JavaDocについては<a href="https://ja.wikipedia.org/wiki/Javadoc">こちら</a>
+ * 
  * @see http://takunoji.hatenablog.com/entry/2015/11/05/184839
  * @author ZenryokuService
  */
-public class Step1_4 {
+/*
+ *  [public class]はどこからでもアクセス可能なクラスである事を示します
+ *  上記に対して[private class] Step1-4.txtの「インナークラス」参照
+ *  　　　　　　[abstract class] Step1-4.txtの「抽象クラス」参照
+ */
+public abstract class Step1_4 {
+
+	/**
+	 * インナークラス
+	 */
+	private class SampleCode1_4 {
+		/** フィールド変数 */
+		private String str = "文字";
+		/**
+		 * 文字を返却する
+		 */
+		public String getStr() {
+			// thisと記述したクラスを示す
+			// この場合はSampleCode4クラス
+			return this.str;
+		}
+	}
+	/**
+	 * サンプルコード１<br/>
+	 * クラスの外からでもアクセスのできる<br/>
+	 * 返却値なし<br/>
+	 * 引数なしのメソッド<br/>
+	 */
+	public void sample1() {
+		System.out.println("サンプル１");
+	}
+
+	/**
+	 * サンプルコード2<br/>
+	 * クラスの内からしかアクセスのできない<br/>
+	 * 返却値はchar型<br/>
+	 * 引数なしのメソッド<br/>
+	 */
+	private char sample2() {
+		System.out.println("サンプル２");
+		return 'A';
+	}
+
+	/**
+	 * サンプルコード３<br/>
+	 * クラスの内からしかアクセスのできない<br/>
+	 * 返却値なし<br/>
+	 * 引数String型のメソッド<br/>
+	 */
+	private void sample3(String str) {
+		System.out.println("サンプル3" + str);
+	}
+	/**
+	 * サンプルコード４<br/>
+	 * 同じパッケージ内、継承関係のあるクラスからしかアクセスのできない<br/>
+	 * 返却値なし<br/>
+	 * 引数、String型<br/>
+	 */
+	protected void sample4(String str) {
+		/* インスタンスを生成し使用する */
+		// インナークラスをインスタンス生成
+		SampleCode1_4 smp = new SampleCode1_4();
+		System.out.println("サンプル4" + smp.getStr());
+	}
+	/**
+	 * サンプルコード5<br/>
+	 * 抽象メソッド<br/>
+	 * 返却値なし<br/>
+	 * 引数String型のメソッド<br/>
+	 * 
+	 * このクラスを継承するクラスでこのメソッドを<br/>
+	 * オーバーライドして使用する
+	 */
+	public abstract void sample5(String str);
 	/**
 	 * Step1-4のサンプルソース<br/>
 	 * 変数flgには true/false のどちらかが入ります<br/>
@@ -44,6 +121,9 @@ public class Step1_4 {
 			// 上記以外の時
 			System.out.println("はずれ");
 		}
+		/***********************
+		 * Switch文の書き方      *
+		 * *********************/
 		/*
 		 * switchぶんでは、beak;をいれないと次のcase文も通ってしまう
 		 */
